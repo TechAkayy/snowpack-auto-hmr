8c780a8180e8662bb931c8043ddc2d6f3629bade
akkayy <73772701+akkayy@users.noreply.github.com>
2021-03-24 01:38:34 +1100

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,8 +1,9 @@
-// Update msg from "Original" to "Updated" & click button on index.html
+// Test: Update msg from "Original" to "Updated" & click button on index.html, the new msg should be displayed automatically
 let demo = { msg: "Original" };
 export { demo };
 
 // HMR
+// Remove this section to re-test with the snowpack-plugin-hmr-inject plugin
 if (import.meta.hot) {
   import.meta.hot.accept(({ module }) => {
     demo = module.demo;
